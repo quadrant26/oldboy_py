e19b8dc8ef549678733e4fe6cf8e9f573eb89062
quadrant26 <quadrant26@163.com>
2016-11-17 18:29:02 +0800

    file_name rename(os)

---
+++ day2/rename.py
@@ -0,0 +1,17 @@
+# -*- coding:utf-8 -*-
+
+import os
+import os.path
+
+# 开发文件替换小程序
+
+if __name__ == "__main__":
+    
+    file_name = raw_input("请输入需要改变的文件名：")
+    if os.path.isfile(file_name):
+        new_filename = raw_input("请输入修改后的文件名：")
+        os.rename(file_name, new_filename)
+    else:
+        print("Your computre does not this %s" % dir)
+    
+    
